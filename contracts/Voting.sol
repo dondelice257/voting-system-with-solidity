8a2802df3f0718e00c4c9ba1d79ca455826518c0
dondelice257 <dondelicedushime@gmail.com>
2024-06-27 09:25:20 +0200

    initial commit

---
+++ contracts/Voting.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint public candidatesCount;
+
+    event CandidateAdded(uint id, string name);
+    event VotedEvent(uint indexed _candidateId);
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        emit CandidateAdded(candidatesCount, _name);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+
+        emit VotedEvent(_candidateId);
+    }
+}
